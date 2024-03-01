0fd4a09712e672bd2cc1ff1388f54f817bf27c2b
MuhummadShohag <muhammadshohagislam.software.e@gmail.com>
2024-03-01 19:01:31 +0600

    [done] fix redis config

--- e-commerce-api-gateway/src/shared/redis.ts
+++ e-commerce-api-gateway/src/shared/redis.ts
@@ -1,15 +1,19 @@
 import { createClient } from 'redis';
-import config from '../config';
+
 import { logger } from './logger';
+import config from '../config';
 
 const redisClient = createClient({
   url: config.redis.url,
 });
 
 redisClient.on('error', error => logger.error('RedisError', error));
-redisClient.on('connect', error =>
-  logger.info('Redis Connected successfully!', error)
-);
+redisClient.on('connect', error => {
+  if (error) {
+    logger.error('RedisError', error);
+  }
+  logger.info('Redis Connected successfully!');
+});
 
 // connect the redis
 const connect = async (): Promise<void> => {
